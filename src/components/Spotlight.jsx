671e640de4f18f5a4c1dfa92a22711d63266901d
MatiasDAppella <appellamatias@gmail.com>
2023-09-28 14:41:37 -0300

    working on main section

--- src/components/Spotlight.jsx
+++ src/components/Spotlight.jsx
@@ -17,7 +17,7 @@ export function Spotlight(){
 
   return (<div
     id="light"
-    className="pointer-events-none absolute h-screen w-screen top-0 left-0 z-50"
+    className="pointer-events-none fixed inset-0 h-screen w-screen top-0 left-0 z-50"
     style={`background: radial-gradient(600px at ${mouseCoordinates.x}px ${mouseCoordinates.y}px, rgba(29, 78, 216, 0.15), transparent 80%);`}>
   </div>)
 };
