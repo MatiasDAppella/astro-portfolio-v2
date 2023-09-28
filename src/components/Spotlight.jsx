ef75025702b1f166e0a3c53dc8c601c4b9e99cb4
MatiasDAppella <appellamatias@gmail.com>
2023-09-28 12:35:39 -0300

    spotlight effect added

---
+++ src/components/Spotlight.jsx
@@ -0,0 +1,23 @@
+import { useEffect, useState } from 'preact/hooks'
+
+export function Spotlight(){
+  const [mouseCoordinates, setMouseCoordinates] = useState({ x: 0, y: 0 })
+
+  const moveHandler = (event) => {
+    setMouseCoordinates({
+      x: event.clientX,
+      y: event.clientY
+    })
+  }
+
+  useEffect(() => {
+    window.addEventListener('mousemove', moveHandler)
+    return (() => window.removeEventListener('mousemove', moveHandler))
+  }, [])
+
+  return (<div
+    id="light"
+    className="pointer-events-none absolute h-screen w-screen top-0 left-0 z-50"
+    style={`background: radial-gradient(600px at ${mouseCoordinates.x}px ${mouseCoordinates.y}px, rgba(29, 78, 216, 0.15), transparent 80%);`}>
+  </div>)
+};
